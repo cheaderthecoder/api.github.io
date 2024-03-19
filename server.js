7d18ddbf8adc741651df91a42b7e90f16d56e21c
cheaderthecoder <143862092+cheaderthecoder@users.noreply.github.com>
2024-03-19 16:54:31 +0300

    Revert "add server stuff"
    This reverts commit fdb547b9d5ff92ca3703a8e43bdbdf32f8ae49dc.

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { server_ } from "https://deno.land/std@0.107.0/http/server.ts";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC6LQuUkER0-ryq40bZMkZ72FLXDUdosf4",
-  authDomain: "test-api-req-app.firebaseapp.com",
-  projectId: "test-api-req-app",
-  storageBucket: "test-api-req-app.appspot.com",
-  messagingSenderId: "119358782725",
-  appId: "1:119358782725:web:873230218085f1731ccb6c"
-};
-
-for await (const request of server) {
-  if (request.method === 'GET') {
-    request.respond({ body: "SUCCSES " });
-  }
-}
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const server_ = serve({ port: 8000 });
-
-console.log("HTTP webserver running on: http://localhost:8000/");
-
-
-
 const http = require('http');
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer((req, res) => {
   if (req.url === '/api.github.io/') {
     if (req.method === 'POST') {
       let body = '';
@@ -48,4 +19,5 @@ const server = http.createServer( (req, res) => {
     res.end('Invalid endpoint');
   }
 });
+
 server.listen(3000);
