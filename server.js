f398093122593ceb0ed26ddf9df07b546e733d7b
cheaderthecoder <143862092+cheaderthecoder@users.noreply.github.com>
2024-03-19 06:47:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/api') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      res.end('<html><body>contents 1</body></html>');
-    });
+  if (req.url === '/api.github.io') {
+    if (req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        res.end('<html><body>contents 1</body></html>');
+      });
+    } else if (req.method === 'GET') {
+      res.end('<html><body>This is a GET request.</body></html>');
+    } else {
+      res.end('Invalid request');
+    }
   } else {
-    res.end('Invalid request');
+    res.end('Invalid endpoint');
   }
 });
 
